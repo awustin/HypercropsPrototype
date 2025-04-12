7e8aae6f112238062a3088dbb2de0632c3b5484c
Agustin Garcia <agustin94garcia@gmail.com>
2025-04-12 19:42:31 +0100

    Added CardButton to Card prefab

--- Assets/Classes/Model/CardButton.cs
+++ Assets/Classes/Model/CardButton.cs
@@ -3,8 +3,6 @@ using UnityEngine.EventSystems;
 
 public class CardButton : UIElementBehaviour, IPointerClickHandler
 {
-    [HideInInspector] public GameEventSender Sender;
-
     override public void OnPointerEnter(PointerEventData data)
     {
         base.OnPointerEnter(data);
@@ -14,23 +12,29 @@ public class CardButton : UIElementBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData data)
     {
         GameObject card = data.pointerPress;
+        Card CardScript = card.GetComponent<Card>();
+        CardType type = CardScript.cardType;
+
         State.SetLastSelected(card);
 
-        // TODO: Handle card behaviour based on type
-        // TODO: Don't use tags or ActionName. Use data packed in GameObjects instead
-        // LastSelected = e.Target;
+        if (type == CardType.Crop)
+        {
+            string cropName = CardScript.name;
+            GameEventSender.Instance.BroadcastFarmingModeEvent(new Vector3(0, 0, 0), cropName);
 
-        // if (e.ActionName == "CropCard")
-        // {
-        //     // Start farming mode
-        //     Card CardScript = e.Target.GetComponent<Card>();
-        //     string cropName = CardScript.name;
+            return;
+        }
 
-        //     Sender.BroadcastFarmingModeEvent(new Vector3(0, 0, 0), cropName);
-        // }
-        // else if (e.ActionName == "AdvanceTime")
-        // {
-        //     Sender.BroadcastAdvanceTimeEvent();
-        // }
+        if (type == CardType.Infrastructure)
+        {
+            // TODO: Start building mode
+            return;
+        }
+        
+        if (type == CardType.Tech)
+        {
+            // TODO: Apply effect
+            return;
+        }
     }
 }
